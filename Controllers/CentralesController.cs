50ab7e1e917405f57e2b4dbb8a87d1241925e158
ClaudioOliveda <coliveda@gmail.com>
2023-04-25 15:47:11 -0600

    Se modifica y se agrega lista de Centrales / Estados de Central / Modificar estado de una Central

--- Controllers/CentralesController.cs
+++ Controllers/CentralesController.cs
@@ -1,6 +1,10 @@
-﻿using Cemsa_BackEnd.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Cemsa_BackEnd.Models;
 
 namespace Cemsa_BackEnd.Controllers
 {
@@ -8,10 +12,13 @@ namespace Cemsa_BackEnd.Controllers
     [ApiController]
     public class CentralesController : ControllerBase
     {
-        //private readonly ApplicationDbContext _context;
-
-        //GET: api/servicios
-        [HttpGet]
+        //GET: api/centrales/obtenerCentrales
+        /// <summary>
+        /// Recupera el listado de Centrales de la Base de datos
+        /// </summary>
+        /// <returns>Lista de Centrales</returns>
+        /// <exception cref="Exception"></exception>
+        [HttpGet("listaCentrales")]
         public async Task<ActionResult<List<TCentral>>> obtenerCentrales()
         {
             try
@@ -27,7 +34,8 @@ namespace Cemsa_BackEnd.Controllers
                                    tc.CenNro,
                                    tc2.CliApeNomDen,
                                    tu.Usuario,
-                                   tec.EstDescripcion
+                                   tec.EstDescripcion,
+                                   tec.EstId
                                }).ToListAsync();
                     return Ok(query);
                 }                  
@@ -35,6 +43,62 @@ namespace Cemsa_BackEnd.Controllers
             catch (Exception ex)
             {
                 throw new Exception("Error al intentar obtener Lista de Centrales", ex);
+            }
+        }
+
+        //POST: api/centrales 
+        /// <summary>
+        /// Modifica estado de una Central
+        /// Si es una Baja agrega la Data sino fechaBaja = NULL
+        /// </summary>
+        /// <returns>Modifica Estado de una Central</returns>
+        /// <exception cref="Exception"></exception>
+        [HttpPost("{cenNum}/{idEstado}")]
+        public async Task<ActionResult> modificarEstado(int cenNum, int idEstado)
+        {
+            try
+            {
+                using (var db = new CemsaContext())
+                {
+                    var central = await db.TCentrals.FirstOrDefaultAsync(c => c.CenNro == cenNum);
+                    if (central != null)
+                    {
+                        central.CenIdEstadoCentral = idEstado;
+                        central.CenFechaBaja = central.CenIdEstadoCentral == 4 ? DateTime.Now : null;
+                        await db.SaveChangesAsync();
+                        return Ok();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar actualizar los datos del Estado de la Central", ex);
+            }
+        }
+
+        //GET: api/centrales/obtenerEstados
+        /// <summary>
+        /// Recupera el listado de Estados de una Central de la Base de datos
+        /// </summary>
+        /// <returns>Lista de Estados de una Central</returns>
+        /// <exception cref="Exception"></exception>
+        [HttpGet("listaEstadosCentrales")]
+        public async Task<List<TEstadoCentral>> obtenerEstados()
+        {
+            try
+            {
+                using (var db = new CemsaContext())
+                {
+                    return await db.TEstadoCentrals.ToListAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al intentar obtener Lista de Estados de Central", ex);
             }
         }
     }
